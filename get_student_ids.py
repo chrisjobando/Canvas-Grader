4bf451a43db5dc2209be0cfd1a3a393ade7fec4d
Chris <chrisjobando@gmail.com>
2020-03-23 11:31:47 -0400

    Added README

--- get_student_ids.py
+++ get_student_ids.py
@@ -10,11 +10,6 @@ from canvasapi import Canvas
 API_URL = config.API_URL
 
 # Canvas API Key
-# To set it up, you need to create a token authorizing the app to access your Canvas courses:
-# 1) In Canvas, select “Account” from the navigation bar on the left, then select “Settings” from the menu that pops out.
-# 2) Scroll down to the “Approval Integrations:” section and click the “+ New Access Token” button. Fill in the popup (the “Purpose” field is just a note to yourself, so use something like “canvasrsync tool”, and the “Expires” field lets you put a limit on how long the token will be valid. I set it to the middle of the summer and re-generate a token every year).
-# 3) Copy the token from the popup. Once you close the window, you cannot get the token again (you have to generate a new one).
-# 4) Add token to config file as a variable
 API_KEY = config.CANVAS_TOKEN
 
 # Initialize a new Canvas object
